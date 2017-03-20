b4efd85cca403332ac2da98e1c7dc6af18d42dff
Sarah Dueweke <sdueweke@gmail.com>
2017-03-21 11:42:40 +1300

    Add new js animation to squares

--- events.js
+++ events.js
@@ -1,5 +1,5 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
@@ -7,32 +7,53 @@ function start () {
   // The first example is done for you. This will change the background colour of the first div
   // when you mouse over it.
   one()
-    
+  two()
+  three()
+  four()
+
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
 function one () {
-  // First, we have to find the element:
   var one = document.getElementById('one')
-
-  // Next, we add an event listener to it:
   one.addEventListener('mouseenter', makeBlue)
-
-  // Finally, we add one to make the colour white again
   one.addEventListener('mouseleave', makeWhite)
 }
 
-// CREATE FUNCTION two HERE
+function two () {
+  var two = document.getElementById('two');
+  two.addEventListener('mouseenter', makeGreen)
+  two.addEventListener('mouseleave', makeWhite)
+}
 
-// CREATE FUNCTION three HERE
+function three () {
+  var three = document.getElementById('three');
+  three.addEventListener('mouseenter', makePurple)
+  three.addEventListener('mouseleave', makeWhite)
+}
 
-// CREATE FUNCTION four HERE
+function four () {
+  var four = document.getElementById('four');
+  four.addEventListener('click', borderBlack)
+}
 
-// Changes the background color of event's target
+
 function makeBlue (evt) {
   evt.target.style.backgroundColor = 'blue'
 }
 
 function makeWhite (evt) {
   evt.target.style.backgroundColor = 'white'
+}
+
+function makeGreen (evt) {
+  evt.target.style.backgroundColor = 'green'
+}
+
+function makePurple (evt) {
+  evt.target.style.backgroundColor = 'purple'
+}
+
+function borderBlack (evt) {
+  evt.target.style.borderColor = "black"
 }
